e16370f4a527683bd8bfd1a48efefa447707a4bf
Banee Ishaque K <Baneeishaque@gmail.com>
2020-05-24 20:31:40 +0530

    Api Calls

--- src/main/java/springboot/basewebapp/SpringBootBaseWebApp.java
+++ src/main/java/springboot/basewebapp/SpringBootBaseWebApp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootBasewebappApplication {
+public class SpringBootBaseWebApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootBasewebappApplication.class, args);
+        SpringApplication.run(SpringBootBaseWebApp.class, args);
     }
 
 }
