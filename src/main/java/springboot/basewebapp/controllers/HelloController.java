b8b8e4d4f4b086d7495c33ff225ebe6c458c1193
Banee Ishaque K <Baneeishaque@gmail.com>
2020-05-24 21:53:20 +0530

    Idea Project - Spring Quick Start

--- src/main/java/springboot/basewebapp/controllers/HelloController.java
+++ src/main/java/springboot/basewebapp/controllers/HelloController.java
@@ -6,11 +6,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-public class MainController {
+public class HelloController {
 
-    @GetMapping("/")
+    @GetMapping("/hello")
     @ResponseBody
-    public String index() {
-        return "Hello World!!!";
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+
+        return String.format("Hello %s!", name);
     }
 }
